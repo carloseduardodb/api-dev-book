7bb92a96eca81cfed78e5178fc700816527edb98
Carlos Eduardo Dias Batista <carloseduardodiasbatista@gmail.com>
2022-08-29 14:30:11 -0300

    feat: validation in user created

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -23,6 +23,11 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if err = user.Prepare(); err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
 	db, err := database.Connect()
 	if err != nil {
 		responses.ERROR(w, http.StatusInternalServerError, err)
--- src/models/users.go
+++ src/models/users.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"strings"
+)
+
 type User struct {
 	ID        uint64 `json:"id,omitempty"`
 	Name      string `json:"name,omitempty"`
@@ -9,4 +14,35 @@ type User struct {
 	CreatedAt string `json:"created_at,omitempty"`
 	UpdatedAt string `json:"updated_at,omitempty"`
 	DeletedAt string `json:"deleted_at,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if err := user.validation(); err != nil {
+		return err
+	}
+	user.format()
+	return nil
+}
+
+func (user *User) validation() error {
+	if user.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if user.Nick == "" {
+		return errors.New("nick is required")
+	}
+	if user.Email == "" {
+		return errors.New("email is required")
+	}
+	if user.Password == "" {
+		return errors.New("password is required")
+	}
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
