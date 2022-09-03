99e8335836e32a78e6d76b50876ea540dd7e2c45
Carlos Eduardo Dias Batista <carloseduardodiasbatista@gmail.com>
2022-09-03 13:57:55 -0300

    feat: add cors management

--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
+	github.com/rs/cors v1.8.2
 	golang.org/x/crypto v0.0.0-20220826181053-bd7e27e6170d
 )
--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func main() {
 	config.Load()
 	r := router.Generate()
+	/* disable cors */
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowCredentials: true,
+	})
+	handler := cors.Handler(r)
 	fmt.Println("Server is running on port:", config.Port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", config.Port), r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", config.Port), handler))
 }
